482a1a839a309f4f11ed00eca1116e3d302bc53f
Engelin Emil <emy394@edu.sollentuna.se>
2019-01-11 12:30:06 +0100

    Update

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,6 @@
-/* add or remove the header "flash" on scroll (if at the top of the page: remove flash) */
+var navExpanded = false; // Is the navbar expanded?
+
+/* Add or remove the header "flash" on scroll (if at the top of the page: remove flash) */
 window.addEventListener('scroll',function() {
     if (window.scrollY == 0) {
         document.getElementById("header").classList.remove("site-top-bar-flash");
@@ -6,3 +8,13 @@ window.addEventListener('scroll',function() {
         document.getElementById("header").classList.add("site-top-bar-flash");
     }
 });
+
+function expandNav() {
+    if (!navExpanded) {
+        document.getElementById("nav-list").classList.add("nav-list--expanded");        
+    } else {
+        document.getElementById("nav-list").classList.remove("nav-list--expanded");
+    }
+
+    navExpanded = !navExpanded;
+}

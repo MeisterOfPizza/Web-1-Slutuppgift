b8d7aaf20e6d121c6c5feec53f85bf379ec917f3
MeisterOfPizza <32549182+MeisterOfPizza@users.noreply.github.com>
2019-01-22 15:58:47 +0100

    Update

--- scripts/facts.js
+++ scripts/facts.js
@@ -13,29 +13,33 @@ function setFactBgColor() {
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
+  
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+
   return color;
 }
 
 function setFactIds() {
   var facts = document.getElementsByClassName("fact");
-  var factContent = document.getElementsByClassName("fact__content");
+  var factContent = document.getElementsByClassName("fact__text__container");
   var factArrows = document.getElementsByClassName("fact__arrow");
 
   for (let i = 0; i < facts.length; i++) {
-    facts[i].onclick = openFact("fact-text-" + i, "fact-arrow-" + i);
+    facts[i].onclick = function() { openFact("fact-text-" + i, "fact-arrow-" + i); };
     factContent[i].id = "fact-text-" + i;
     factArrows[i].id = "fact-arrow-" + i;
   }
 }
 
 function openFact(id, arrowId) {
-  var elem = document.getElementById(id, arrowId);
+  var elem = document.getElementById(id);
   var content = elem.firstElementChild;
 
-  document.getElementById(arrowId).innerHTML = elem.classList.contains("open") ? "&#xe5c5;" : "&#xe5c7;";
+  //document.getElementById(arrowId).innerHTML = elem.classList.contains("open") ? "&#xe5c5;" : "&#xe5c7;";
+
+  document.getElementById(arrowId).classList.toggle("open");
 
   if (elem.classList.contains("open")) {
     elem.classList.remove("open");

adcfe29862ba7a71dc9ed4585a7542cdadf3b0f7
Emil Engelin <emil.engelin@gmail.com>
2019-02-13 12:28:58 +0100

    Fixes
    Fixed warnings and errors.
    Added an orange tint to active action buttons on image filter (gallery).

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function setActions() {
-    var items = document.getElementsByClassName("gallery-item");
+    var items = document.getElementsByClassName("gallery-item"); //Get each gallery item.
 
     for (let i = 0; i < items.length; i++) {
         const element = items[i];
@@ -11,24 +11,36 @@ function setActions() {
         element.getElementsByClassName("gallery-item__img")[0].id = i.toString();
         var actions = element.getElementsByClassName("action");
 
+        //Give each <a> element a name.
+        for (let j = 0; j < actions.length; j++) {
+            actions[j].id = i + ":action_" + j;
+        }
+
+        //Set onclick events for each <a> element.
         if (actions !== undefined && actions.length > 0) {
-            actions[0].onclick = function() { setFilter(i, "hue-rotate(180deg)") };
-            actions[1].onclick = function() { setFilter(i, "invert(100%)") };
-            actions[2].onclick = function() { setFilter(i, "saturate(50%)") };
-            actions[3].onclick = function() { setFilter(i, "") };
-            actions[4].onclick = function() { setFilter(i, "grayscale(100%)") };
-            actions[5].onclick = function() { setFilter(i, "blur(1px)") };
-            actions[6].onclick = function() { setFilter(i, "sepia(100%)") };
+            actions[0].onclick = function() { setFilter(i, "hue-rotate(180deg)", i + ":action_0") };
+            actions[1].onclick = function() { setFilter(i, "invert(100%)", i + ":action_1") };
+            actions[2].onclick = function() { setFilter(i, "saturate(50%)", i + ":action_2") };
+            actions[3].onclick = function() { setFilter(i, "", i + ":action_3") };
+            actions[4].onclick = function() { setFilter(i, "grayscale(100%)", i + ":action_4") };
+            actions[5].onclick = function() { setFilter(i, "blur(1px)", i + ":action_5") };
+            actions[6].onclick = function() { setFilter(i, "sepia(100%)", i + ":action_6") };
         }
     }
 }
 
-function setFilter(id, filter) {
+function setFilter(id, filter, actionId) {
     var img = document.getElementById(id);
 
     //Return if we want to reset it:
     if (filter === "") {
         img.style = "";
+
+        var familyId = actionId.substring(0, actionId.indexOf('_') + 1); //Remove the last id number after the '_'.
+        for (let i = 0; i < 7; i++) {
+            document.getElementById(familyId + i).children[0].style.color = "white"; //Reset the color of the icon.
+        }
+
         return;
     }
 
@@ -36,8 +48,10 @@ function setFilter(id, filter) {
 
     if (style.includes(filter)) {
         style = style.replace(filter, ""); //Remove already applied filter
+        document.getElementById(actionId).children[0].style.color = "white"; //Make the icon white (default).
     } else {
         style += filter; //Add filter
+        document.getElementById(actionId).children[0].style.color = "#ff5703"; //Give the icon a nice orange tint.
     }
 
     img.style = "filter: " + style;
